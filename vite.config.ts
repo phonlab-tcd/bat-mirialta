f23dcb5cefeebe952f3612af83773b5adc57a6d9
john <johnsloan1984@gmail.com>
2023-01-19 15:31:02 +0000

    changed build path back to original

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  build: {
-    outDir: './dist/qa/bat/', // so it can be displayed on abair.ie/qa/bat/
-  },
+  // build: {
+  //   outDir: './dist/qa/bat/', // so it can be displayed on abair.ie/qa/bat/
+  // },
 });
