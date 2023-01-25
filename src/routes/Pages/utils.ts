707f76ac10b36a7add4930d47fd6c4fe0894ad8b
JohnSloan8 <johnsloan1984@gmail.com>
2023-01-25 18:25:59 +0000

    changed textbox so visible on mobile

--- src/routes/Pages/utils.ts
+++ src/routes/Pages/utils.ts
@@ -2,7 +2,7 @@ import type { Theme } from '@mui/material';
 
 function getPageHeight(theme: Theme) {
   const topSpacing = Number(theme.mixins.toolbar.minHeight) + parseInt(theme.spacing(1));
-  return `calc(100vh - ${topSpacing}px)`;
+  return `calc(90vh - ${topSpacing}px)`;
 }
 
 export { getPageHeight };
